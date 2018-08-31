ec40f9908876ddcbaf236f05e3fe88589e265c4b
Michael Bentley <mikebentley15@gmail.com>
2018-08-31 01:23:16 -0700

    lulesh-wrap: stub out static and inline keywords to empty

--- flit-tests/tests/lulesh-wrap.cpp
+++ flit-tests/tests/lulesh-wrap.cpp
@@ -16,10 +16,30 @@ using std::exit_stub;
 
 //void exit_stub(int exit_code) { std::exit_stub(exit_code); }
 
-#define main lulesh_main
+#include <climits>
+#include <vector>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <sys/time.h>
+#include <iostream>
+#include <unistd.h>
 #include <cstdlib>
+
+#if _OPENMP
+# include <omp.h>
+#endif
+
+#define main lulesh_main
 #define exit exit_stub
+#define static
+#define inline
 #include "lulesh.cc"
+#undef inline
+#undef static
 #undef exit
-#undef maiin
+#undef main
 
